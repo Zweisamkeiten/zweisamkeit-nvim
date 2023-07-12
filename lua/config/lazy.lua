87811556a41852c19af369ab5a49e3d13a86ff36
Einsam <snitxmhm@gmail.com>
2023-07-12 22:55:24 +0800

    [lsp](c/cpp): clangd lsp support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+
+    -- import my lsp configuration
+    { import = "langs" },
+
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.

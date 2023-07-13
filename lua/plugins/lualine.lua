950299a4e9a2c5872573037da12d77c75d7767c6
Einsam <snitxmhm@gmail.com>
2023-07-13 20:45:56 +0800

    [plugin](lualine): add lsp & progress format lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,55 @@
+local window_width_limit = 100
+
+local conditions = {
+  buffer_not_empty = function()
+    return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+  end,
+  hide_in_width = function()
+    return vim.o.columns > window_width_limit
+  end,
+  -- check_git_workspace = function()
+  --   local filepath = vim.fn.expand "%:p:h"
+  --   local gitdir = vim.fn.finddir(".git", filepath .. ";")
+  --   return gitdir and #gitdir > 0 and #gitdir < #filepath
+  -- end,
+}
+
+local progress = {
+  "progress",
+  fmt = function()
+    return "%P/%L"
+  end,
+  color = {},
+}
+
+local lsp = {
+  "lsp",
+  fmt = function()
+    local msg = "No Active Lsp"
+    local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+      return msg
+    end
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+        return client.name
+      end
+    end
+    return msg
+  end,
+  icon = ":",
+  color = { fg = "#c678dd", gui = "bold" },
+}
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, lsp)
+      opts.sections.lualine_z = { progress }
+    end,
+  },
+}

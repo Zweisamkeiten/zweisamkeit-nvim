7b49cda72d144999fc073e255f6c2a7da41240ae
Einsam <snitxmhm@gmail.com>
2023-07-15 17:56:38 +0800

    [options]: fold enable

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -57,3 +57,5 @@ vim.g.markdown_recommended_style = 0
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"

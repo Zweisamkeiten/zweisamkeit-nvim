67568e8015b7666046e78ace5005e047678dda83
Einsam <snitxmhm@gmail.com>
2023-09-21 09:06:40 +0800

    [langs]: haskell lsp

--- lua/langs/haskell.lua
+++ lua/langs/haskell.lua
@@ -1,21 +1,21 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    ft = { "haskell", "cabal" },
-    opts = {
-      servers = {
-        hls = {},
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   ft = { "haskell", "cabal" },
+  --   opts = {
+  --     servers = {
+  --       hls = {},
+  --     },
+  --   },
+  -- },
   {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       if type(opts.sources) == "table" then
         local nls = require("null-ls")
-        vim.list_extend({
+        vim.list_extend(opts.sources, {
           nls.builtins.formatting.fourmolu,
-        }, opts.sources)
+        })
       end
     end,
   },

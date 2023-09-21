77c329c25b31dea337e8c29ef12e5eee683638c9
Einsam <snitxmhm@gmail.com>
2023-09-21 09:06:56 +0800

    [config]: disable autoformat for lua and c

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -92,3 +92,10 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- Disable autoformat for lua files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "lua", "c" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})

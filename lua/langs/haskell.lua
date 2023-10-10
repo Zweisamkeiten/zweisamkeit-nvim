2a0a43632dcc40985c38fc2fa26027d37c8aa34b
Einsam <snitxmhm@gmail.com>
2023-10-10 16:10:19 +0800

    [langs]: null-ls.nvim rename

--- lua/langs/haskell.lua
+++ lua/langs/haskell.lua
@@ -9,7 +9,7 @@ return {
   --   },
   -- },
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/null-ls.nvim",
     opts = function(_, opts)
       if type(opts.sources) == "table" then
         local nls = require("null-ls")
